45af08f0b5db4caf6b64c5d42b436f55ba6dba5f
Sofienne LASSOUED <soflass1293@gmail.com>
2019-08-08 00:53:02 +0100

    Add: Added test file.

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const Greetings = require("./classes/Greetings")
 const greet = new Greetings("Sofienne");
 
 greet.sayHello();
+
+const sum = (a, b) => a + b
+module.exports = sum;
---
+++ index.spec.js
@@ -0,0 +1,6 @@
+const sum = require("./index")
+describe('', () => {
+    it("should have correct values", () => {
+        expect(sum(1, 2)).toBe(3)
+    })
+});
